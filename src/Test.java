6f3467f8f18319acf22ca5d3158898955772a077
Marina Korol <korol.marina.88@mail.ru>
2017-11-11 20:03:39 +0300

    Занятие 1

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Марина on 11.11.2017.
+ */
+public class Test {
+    public static void main (String [] args){
+        String today1 = "поэтому выходной.";
+        String today2 = "но я работаю.";
+
+     System.out.println ("Сегодня суббота," +today1);
+     System.out.println ("Сегодня суббота," +today2);
+    }
+}
